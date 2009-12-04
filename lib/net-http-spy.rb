cf0ebebb0627b40c85c67e0e377291e96bdfad51
Greg Weber <greg@grubtop.(none)>
2009-12-04 09:17:42 -0800

    add options for limits to logging

--- lib/net-http-spy.rb
+++ lib/net-http-spy.rb
@@ -15,9 +15,11 @@ module Net
 
     def initialize(*args, &block)
       self.class.http_logger_options ||= {}
-      defaults =  {:body => false, :trace => false, :verbose => false}
+      defaults =  {:body => false, :trace => false, :verbose => false, :limit => -1}
       self.class.http_logger_options = (self.class.http_logger_options == :default) ? defaults : self.class.http_logger_options
       @logger_options = defaults.merge(self.class.http_logger_options)
+      @params_limit = @logger_options[:params_limit] || @logger_options[:limit]
+      @body_limit   = @logger_options[:body_limit]   || @logger_options[:limit]
       
       self.class.http_logger.info "CONNECT: #{args.inspect}" if !@logger_options[:verbose]
 
@@ -35,9 +37,9 @@ module Net
       result = old_request(*args, &block)
       unless started? || @logger_options[:verbose]
 
-        self.class.http_logger.info "PARAMS #{CGI.parse(args[0].body).inspect} " if args[0].body && req != 'CONNECT'
+        self.class.http_logger.info "PARAMS #{CGI.parse(args[0].body).inspect[0..@params_limit]} " if args[0].body && req != 'CONNECT'
         self.class.http_logger.info "TRACE: #{caller.reverse}" if @logger_options[:trace]
-        self.class.http_logger.info "BODY: #{  @logger_options[:body] ? result.body : result.class.name}"
+        self.class.http_logger.info "BODY: #{(@logger_options[:body] ? result.body : result.class.name)[0..@body_limit]}"
       end
       result
     end
