c3747fc03f6e97a3fb53f30182df8c92fd0f132b
msaspence <matthew.spence@fruitbowlmedia.com>
2012-07-03 09:41:37 +0100

    Added time logging

--- lib/net-http-spy.rb
+++ lib/net-http-spy.rb
@@ -20,7 +20,6 @@ module Net
       @logger_options = defaults.merge(self.class.http_logger_options)
       @params_limit = @logger_options[:params_limit] || @logger_options[:limit]
       @body_limit   = @logger_options[:body_limit]   || @logger_options[:limit]
-      
       self.class.http_logger.info "CONNECT: #{args.inspect}" if !@logger_options[:verbose]
 
       old_initialize(*args, &block)
@@ -34,12 +33,15 @@ module Net
         self.class.http_logger.info "#{req} #{args[0].path}"
       end
 
+      time_started = Time.now
       result = old_request(*args, &block)
+      time_taken = Time.now - time_started
       unless started? || @logger_options[:verbose]
 
         self.class.http_logger.info "PARAMS #{CGI.parse(args[0].body).inspect[0..@params_limit]} " if args[0].body && req != 'CONNECT'
         self.class.http_logger.info "TRACE: #{caller.reverse}" if @logger_options[:trace]
         self.class.http_logger.info "BODY: #{(@logger_options[:body] ? result.body : result.class.name)[0..@body_limit]}"
+        self.class.http_logger.info "TIME: #{(time_taken.to_f*1000).round}ms"
       end
       result
     end
